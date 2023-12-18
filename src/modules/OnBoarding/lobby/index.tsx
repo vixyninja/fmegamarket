c9223aa78dff3030b86e3384fae8055d6a4d09c6
vixyninja <nevergiveup2k3@gmail.com>
2023-12-18 13:31:12 +0700

    a

--- src/modules/OnBoarding/lobby/index.tsx
+++ src/modules/OnBoarding/lobby/index.tsx
@@ -30,7 +30,6 @@ export default function LobbyScreen({ navigation }: Props) {
     onRightPress: () => {},
     rightButtonTitle: "Try again",
   });
-
   const onClickSignInEmail = useCallback(() => {
     navigation.navigate("SIGN_IN_SCREEN");
   }, []);
